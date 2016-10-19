2c02c8aba95ad0f43e8300945158d210269dbd13
Arfaniel <dn120284aio@gmail.com>
2016-10-19 13:46:17 +0300

    New Proj

---
+++ Login_regex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Login_regex
+{
+    class Program
+    {
+        public static bool logCheck(string str)
+        {
+            Regex letters = new Regex(@"[A-Z][a-z][0-9][\W]+");
+
+            Match m = letters.Match(str);
+            if (m.Success)
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string temp = "atael01D!";
+            Console.WriteLine(logCheck(temp));
+        }
+    }
+}
